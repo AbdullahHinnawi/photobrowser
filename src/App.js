7009a1b772d83cc77bf22930d67a9045152e7ce3
AbdullahHinnawi <hinnawiabdullah@gmail.com>
2021-01-27 12:43:02 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import NavigationBar from './components/NavigationBar';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import {Gallery} from './components/Gallery';
