63dddb94ef67d0fe5b10f1ab452a42eb008fc4e4
AbdullahHinnawi <hinnawiabdullah@gmail.com>
2021-01-27 12:48:59 +0200

    Deploy to heroku

--- src/services/photoService.js
+++ src/services/photoService.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const getPhotos =  () => {
-  const request =  axios.get('http://jsonplaceholder.typicode.com/photos');
+  const request =  axios.get('https://jsonplaceholder.typicode.com/photos');
   return request.then(res => res.data);
 };
 
 const getPhotoById =  (id) => {
-  const request =  axios.get('http://jsonplaceholder.typicode.com/photos/'+id);
+  const request =  axios.get('https://jsonplaceholder.typicode.com/photos/'+id);
   return request.then(res => res.data);
 };
 
